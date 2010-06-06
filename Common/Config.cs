63f50209d971be5e5b5778e12ead2f5f2642e87a
inga-lovinde <52715130+inga-lovinde@users.noreply.github.com>
2010-06-06 08:47:24 +0000

    Follow-up to previous commit

---
+++ Common/Config.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FLocal.Common {
+
+	public class Config : FLocal.Core.Config<Config> {
+
+		protected Config() : base() {
+		}
+
+		public static void Init() {
+			doInit(() => new Config());
+		}
+
+	}
+
+}
