11e2cda727df9dca9e315ce387dca39d7e9125c2
inga-lovinde <52715130+inga-lovinde@users.noreply.github.com>
2010-06-09 14:52:53 +0000

    MySQL test added for MySQL bug #54329 (see http://bugs.mysql.com/bug.php?id=54329 )

---
+++ tmp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace tmp {
+	class Program {
+		static void Main(string[] args) {
+			string fieldToSelect = args[0];
+			using(MySqlConnection connection = new MySqlConnection("Protocol=pipe;Charset=utf8;Database=flocal_production;Username=flocalp;Password=flocalp")) {
+				connection.Open();
+				using(MySqlCommand command = connection.CreateCommand()) {
+					command.CommandText = "SELECT `" + fieldToSelect + "` FROM `boards`";
+					Console.WriteLine("Command: " + command.CommandText);
+					using(MySqlDataReader reader = command.ExecuteReader()) {
+						Console.WriteLine("Command executed");
+						while(reader.Read()) {
+							Console.WriteLine("Data read");
+						}
+					}
+				}
+			}
+		}
+	}
+}
