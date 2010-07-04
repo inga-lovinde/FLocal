f7770e09fc6b5e49c8d8825a8fa123b8b203dfa1
inga-lovinde <52715130+inga-lovinde@users.noreply.github.com>
2010-07-04 07:43:31 +0000

    Old smileys redirection implemented

--- IISMainHandler/HandlersFactory.cs
+++ IISMainHandler/HandlersFactory.cs
@@ -22,6 +22,9 @@ namespace FLocal.IISHandler {
 			if(context.httprequest.Path.EndsWith(".php")) {
 				return new handlers.response.LegacyPHPHandler();
 			}
+			if(context.httprequest.Path.ToLower().StartsWith("/images/graemlins/")) {
+				throw new RedirectException("/static/smileys/" + context.requestParts[2]);
+			}
 			#endregion
 
 			switch(context.requestParts[0].ToLower()) {
