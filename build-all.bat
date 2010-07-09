5c9e6c4c40b47beee3401d2d547436751741a00c
inga-lovinde <52715130+inga-lovinde@users.noreply.github.com>
2010-07-09 19:28:17 +0000

    Build order changed

--- build-all.bat
+++ build-all.bat
@@ -1,9 +1,9 @@
 @echo off
 @rem Note that this script will only produce a .msi packet of your binaries and data. It will not compile your sources.
 cd Builder
-Builder IISMainHandler debug
-move /Y IISMainHandler\product-debug.msi ..\IISMainHandler-debug.msi
 Builder IISMainHandler release
 move /Y IISMainHandler\product-release.msi ..\IISMainHandler-release.msi
+Builder IISMainHandler debug
+move /Y IISMainHandler\product-debug.msi ..\IISMainHandler-debug.msi
 rem Builder IISUploadHandler release
 rem move /Y IISUploadHandler\product-release.msi ..\IISUploadHandler-release.msi
