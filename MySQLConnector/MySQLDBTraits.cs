575ce0aa59bcae040d7dc875153fb7bf9d75a567
inga-lovinde <52715130+inga-lovinde@users.noreply.github.com>
2010-10-28 20:58:33 +0000

    MySQLConnector now compiles in absence of MySqlClient library

--- MySQLConnector/MySQLDBTraits.cs
+++ MySQLConnector/MySQLDBTraits.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Data.Common;
-using MySql.Data.MySqlClient;
+//using MySql.Data.MySqlClient;
 using FLocal.Core.DB;
 
 namespace FLocal.MySQLConnector {
+	/*
 	public class MySQLDBTraits : IDBTraits {
 
 		public static readonly MySQLDBTraits instance = new MySQLDBTraits();
@@ -33,4 +34,5 @@ namespace FLocal.MySQLConnector {
 		}
 
 	}
+	*/
 }
