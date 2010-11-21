b368f18b67776a423f647f0e88a77ad9e6e61833
inga-lovinde <52715130+inga-lovinde@users.noreply.github.com>
2010-11-21 17:20:22 +0000

    AccessDeniedException finally added to svn

---
+++ Core/exceptions/AccessDeniedException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FLocal.Core {
+
+    public partial class AccessDeniedException : FLocalException {
+
+        public AccessDeniedException(string Message) : base(Message) { }
+
+		public AccessDeniedException() : this("Access denied") { }
+
+    }
+
+}
