84fb0e41bd727b14cf96e751dab334982638bf27
inga-lovinde <52715130+inga-lovinde@users.noreply.github.com>
2012-01-02 01:00:17 +0000

    Files for previous commit

---
+++ Web.Core/Counter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Web.Core {
+	public class Counter {
+
+		private int value;
+		private readonly object locker = new object();
+
+		public Counter() {
+			this.value = 0;
+		}
+
+		public int GetCurrentValueAndIncrement() {
+			lock(this.locker) {
+				int result = this.value;
+				this.value++;
+				return result;
+			}
+		}
+
+	}
+}
---
+++ Web.Core/ILogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Web.Core {
+	public interface ILogger {
+
+		/// <summary>
+		/// </summary>
+		/// <param name="message">Single-line message</param>
+		void Log(string message);
+
+	}
+}
