c32ac15925364df881bd9f661bbaff014be3f5b6
inga-lovinde <52715130+inga-lovinde@users.noreply.github.com>
2012-04-20 22:45:42 +0400

    User references appearance improved

--- FLocal.Common/BBCodes/helpers/UserMentionProcessor.cs
+++ FLocal.Common/BBCodes/helpers/UserMentionProcessor.cs
@@ -14,7 +14,7 @@ namespace FLocal.Common.BBCodes {
 			var user = dataobjects.User.LoadByName(username);
 			context.OnUserMention(user);
 			var url = new URL.users.user.Info(user.id.ToString(), null);
-			return String.Format("<a class=\"separate UG_{0}\" href=\"{1}\">{2}</a>", Safe(user.userGroup.name), url.canonical, Safe(user.name));
+			return String.Format("<a class=\"separate userreference UG_{0}\" href=\"{1}\">{2}</a>", Safe(user.userGroup.name), url.canonical, Safe(user.name));
 		}
 
 	}
